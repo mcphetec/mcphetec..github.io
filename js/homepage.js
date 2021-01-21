3000072ffe4e797d706ac195dcfc05509a34aac4
mcphetec <cameron.mcpheters@gmail.com>
2021-01-21 15:45:10 -0700

    updated all

---
+++ js/homepage.js
@@ -0,0 +1,10 @@
+
+var string = document.lastModified;
+
+document.getElementById("updated").textContent = string;
+
+let d= new Date();
+var year = d.getFullYear();
+
+document.getElementById("copyright").textContent = year;
+
